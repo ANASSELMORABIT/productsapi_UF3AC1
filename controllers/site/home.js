56b3fe2d48f86cd43943fead387f7960c38397dc
ANASETEL <148634360+ANASETEL@users.noreply.github.com>
2025-03-17 21:22:14 +0100

    Añadir el slugify

--- controllers/site/home.js
+++ controllers/site/home.js
@@ -1,23 +1,25 @@
-const renderHome = (req,res) => {
-    titulo = "Wellcome to Products API"
-    aviso = "Register to get API key!"
-    howto = "(to provide it, place it on header 'api-key' or as URL param 'apikey')"
-    rutas = [
-        ["Register","POST /api/users/register"],
-        ["List all products","GET /api/products"],
-        ["List products by ID","GET /api/products/:productID"],
-        ["Create product","POST /api/products"],
-        ["Update product","PUT /api/products/:productID"],
-        ["Delete product","DELETE /api/products/:productID"]
-    ]
-  
-    res.render('index.pug',
-    {  
+const slugify = require('slugify');
+
+const renderHome = (req, res) => {
+    const titulo = slugify("Wellcome to Products API", { replacement: '*', lower: true });
+    const aviso = "Register to get API key!";
+    const howto = "(to provide it, place it on header 'api-key' or as URL param 'apikey')";
+    const rutas = [
+        ["Register", "POST /api/users/register"],
+        ["List all products", "GET /api/products"],
+        ["List products by ID", "GET /api/products/:productID"],
+        ["Create product", "POST /api/products"],
+        ["Update product", "PUT /api/products/:productID"],
+        ["Delete product", "DELETE /api/products/:productID"]
+    ];
+
+    res.render('index.pug', {
         titulo: titulo,
         aviso: aviso,
         rutas: rutas
-    })
-}
+    });
+};
 
-module.exports = renderHome
+module.exports = renderHome;
+
 
